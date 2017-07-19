68adfbb6dada1a308c0183bdcfe0527d15805cef
Silaev Konstantin < @sil-ka.NHP.local>
2017-07-19 18:20:50 +0500

    All

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		//JetInstance instance1 = Jet.newJetInstance();
+
+		//DemoApplication
+		SpringApplication.run(String.class, args);
+	}
+}
